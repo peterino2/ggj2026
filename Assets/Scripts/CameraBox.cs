73b16e31b9c10682b542930718a39226b5de35e6
absolutionxi <lancewiffen@hotmail.ca>
2026-01-30 22:21:06 -0800

    convert to sprite for player model, camerabox adjustments, random misc.

--- Assets/Scripts/CameraBox.cs
+++ Assets/Scripts/CameraBox.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
 
 public class CameraBox : MonoBehaviour
 {
-    private Camera cam;
+    public Camera cam;
+    public Vector2 playSize;
     private BoxCollider2D camBox;
     private float sizex, sizey, ratio;
 
     void Start()
-    {
+    { 
         cam = GetComponent<Camera>();
         camBox = GetComponent<BoxCollider2D>();
     }
