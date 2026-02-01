d087fa4b9f801105d4221bd6831dc20c33b6d05a
Norman Wong <norman_wong@live.com>
2026-01-31 16:36:50 -0800

    Laser gun

---
+++ Assets/Scripts/Weapons/LaserBeam.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    float CurrentPower = 0.0f;
+
+    public void SetCurrentPower(float Power)
+    {
+        CurrentPower = Power;
+        gameObject.transform.localScale = new Vector3(1.0f, CurrentPower, 1.0f);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
